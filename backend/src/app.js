c2f94d2669d18df5c4893031bb0880206f9cb0fd
Javier Jara <jara.javier.jtv@gmail.com>
2025-12-12 02:37:15 -0700

    si

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require("express");
 const productRoutes = require("./routes/product.routes");
-const shoppingCartRoutes = require("./routes/shoppingCart.routes");
+const shoppingCartRoutes = require("./routes/shoppingcart.routes");
 const authRoutes = require("./routes/auth.routes")
 const cors = require('cors');
 const app = express();
