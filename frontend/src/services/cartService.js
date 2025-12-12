36755a3ecc8c03d0cc69abe50c65f5c53ae3738e
Javier Jara <jara.javier.jtv@gmail.com>
2025-12-12 01:01:45 -0700

    Static + url backend

--- frontend/src/services/cartService.js
+++ frontend/src/services/cartService.js
@@ -1,5 +1,5 @@
 // services/cartService.js
-const API_URL = 'http://localhost:3000/api';
+const API_URL = 'https://ecommerce-dsw.onrender.com';
 
 const getAuthHeaders = () => {
     const token = localStorage.getItem('token');
