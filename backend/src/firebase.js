7b6f49003a053705e71ef3f6c928f3d6d5250cce
RuiloAbyss <ruilo.abisai@outlook.com>
2025-12-18 13:09:18 -0700

    .

--- backend/src/firebase.js
+++ backend/src/firebase.js
@@ -5,7 +5,6 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  // El .replace es necesario para procesar correctamente los saltos de línea (\n)
   private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
