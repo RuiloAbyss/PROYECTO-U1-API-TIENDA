76d6bc8712f7ac43fa64b218277a110d0936fe3a
RuiloAbyss <ruilo.abisai@outlook.com>
2025-12-18 19:55:11 -0700

    url FIXED?

--- frontend/src/pages/config.js
+++ frontend/src/pages/config.js
@@ -1,3 +1,5 @@
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_BASE_URL = import.meta.env.DEV 
+  ? 'http://localhost:3000'                      // URL para pruebas locales
+  : 'https://electronic-store-ruiloop.vercel.app'; // URL de tu backend en producción
 
 export default API_BASE_URL;
